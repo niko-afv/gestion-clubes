9a6eba0acf533c1bc25343049d386e6324ef9463
nfredes <nfredes@grouponlatam.com>
2019-04-24 20:49:30 -0400

    Dashboard view updated with info to Regional profile

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,6 +3,8 @@
 @section('content')
 <div class="wrapper wrapper-content">
     <div class="row">
+
+        @if(Auth::user()->profile->level >= 3)
         <div class="col-lg-3">
             <div class="ibox ">
                 <div class="ibox-title">
@@ -54,6 +56,59 @@
                 </div>
             </div>
         </div>
+        @else
+            <div class="col-lg-3">
+                <div class="ibox ">
+                    <div class="ibox-title">
+                        <!--
+                        <span class="label label-success float-right"></span>
+                        -->
+                        <h5>Clubes</h5>
+                    </div>
+                    <div class="ibox-content">
+                        <h1 class="no-margins">{{ \App\Club::all()->count() }}</h1>
+                        <!--
+                        <div class="stat-percent font-bold text-success">98% <i class="fa fa-bolt"></i></div>
+                        -->
+                        <small>Clubes registrados</small>
+                    </div>
+                </div>
+            </div>
+            <div class="col-lg-3">
+                <div class="ibox ">
+                    <div class="ibox-title">
+                        <!--
+                        <span class="label label-success float-right"></span>
+                        -->
+                        <h5>Unidades</h5>
+                    </div>
+                    <div class="ibox-content">
+                        <h1 class="no-margins">{{ \App\Unit::all()->count() }}</h1>
+                        <!--
+                        <div class="stat-percent font-bold text-success">98% <i class="fa fa-bolt"></i></div>
+                        -->
+                        <small>Unidades registradas</small>
+                    </div>
+                </div>
+            </div>
+            <div class="col-lg-3">
+                <div class="ibox ">
+                    <div class="ibox-title">
+                        <!--
+                        <span class="label label-success float-right"></span>
+                        -->
+                        <h5>Eventos</h5>
+                    </div>
+                    <div class="ibox-content">
+                        <h1 class="no-margins">{{ \App\Event::all()->count() }}</h1>
+                        <!--
+                        <div class="stat-percent font-bold text-success">98% <i class="fa fa-bolt"></i></div>
+                        -->
+                        <small>Eventos creados</small>
+                    </div>
+                </div>
+            </div>
+        @endif
     </div>
 
     <!--
