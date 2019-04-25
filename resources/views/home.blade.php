f84076e601436e6ab3caa4d792a4582e313c1801
nfredes <nfredes@grouponlatam.com>
2019-04-24 20:44:10 -0400

    Dashboard view updated with info

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,7 +1,62 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
+<div class="wrapper wrapper-content">
+    <div class="row">
+        <div class="col-lg-3">
+            <div class="ibox ">
+                <div class="ibox-title">
+                    <!--
+                    <span class="label label-success float-right"></span>
+                    -->
+                    <h5>Miembros</h5>
+                </div>
+                <div class="ibox-content">
+                    <h1 class="no-margins">{{ Auth::user()->member->institutable->members()->count() }}</h1>
+                    <!--
+                    <div class="stat-percent font-bold text-success">98% <i class="fa fa-bolt"></i></div>
+                    -->
+                    <small>Miembros registrados</small>
+                </div>
+            </div>
+        </div>
+        <div class="col-lg-3">
+            <div class="ibox ">
+                <div class="ibox-title">
+                    <!--
+                    <span class="label label-success float-right"></span>
+                    -->
+                    <h5>Unidades</h5>
+                </div>
+                <div class="ibox-content">
+                    <h1 class="no-margins">{{ Auth::user()->member->institutable->units()->count() }}</h1>
+                    <!--
+                    <div class="stat-percent font-bold text-success">98% <i class="fa fa-bolt"></i></div>
+                    -->
+                    <small>Unidades registradas</small>
+                </div>
+            </div>
+        </div>
+        <div class="col-lg-3">
+            <div class="ibox ">
+                <div class="ibox-title">
+                    <!--
+                    <span class="label label-success float-right"></span>
+                    -->
+                    <h5>Eventos</h5>
+                </div>
+                <div class="ibox-content">
+                    <h1 class="no-margins">{{ 0 }}</h1>
+                    <!--
+                    <div class="stat-percent font-bold text-success">98% <i class="fa fa-bolt"></i></div>
+                    -->
+                    <small>Participaciones en Eventos</small>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <!--
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card">
@@ -19,5 +74,7 @@
             </div>
         </div>
     </div>
+    -->
 </div>
+
 @endsection
