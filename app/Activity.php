e50b9e9c07e5771ccb2308186f1a745a0ed84d27
nfredes <nfredes@grouponlatam.com>
2019-05-03 02:37:52 -0400

    relationship between activity and category added

--- app/Activity.php
+++ app/Activity.php
@@ -11,4 +11,8 @@ class Activity extends Model
     public function event(){
         return $this->belongsTo(Event::class);
     }
+
+    public function category(){
+        return $this->belongsTo(ActivityCategory::class);
+    }
 }
