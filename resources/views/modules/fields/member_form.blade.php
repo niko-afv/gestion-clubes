7621b98f8132524bbb83bc76f4b325f40ddd050c
nfredes <nfredes@grouponlatam.com>
2019-08-16 14:18:57 -0400

    fix title

--- resources/views/modules/fields/member_form.blade.php
+++ resources/views/modules/fields/member_form.blade.php
@@ -1,6 +1,6 @@
 @extends('layouts.app')
 
-@section('title', (isset($member))?'Modificar Miembro':'Crear Miembro')
+@section('title', 'Modificar/Crear Miembro')
 
 @section('content')
     <div class="row wrapper border-bottom white-bg page-heading">
