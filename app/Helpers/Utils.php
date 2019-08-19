3dc26412b5444198f4ea0a58c10f0ea5a3c032c5
nfredes <nfredes@grouponlatam.com>
2019-08-19 09:40:58 -0400

    add status label on club inscription

---
+++ app/Helpers/Utils.php
@@ -0,0 +1,24 @@
+<?php
+
+    function participationStatusAsLabel($status)
+    {
+        switch ($status){
+            case \App\Enums\ParticipationStatus::INCOMPLETED_INSCRIPTION():
+                $type = 'danger';
+                break;
+            case \App\Enums\ParticipationStatus::COMPLETED_INSCRIPTION():
+                $type = 'primary';
+                break;
+            case \App\Enums\PaymentStatus::PENDING():
+                $type = 'danger';
+                break;
+            case \App\Enums\PaymentStatus::PAYED():
+                $type = 'warning';
+                break;
+            case \App\Enums\PaymentStatus::VERIFIED():
+                $type = 'primary';
+                break;
+        }
+
+        return ['type'=> $type, 'status' => $status];
+    }
